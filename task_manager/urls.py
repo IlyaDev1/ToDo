3bcc56a376f43af96b626f3a5b0b2dbd437f4138
IliDevia <neiluamaibi@mail.ru>
2024-09-18 21:36:16 +0300

    AddCreateView

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import TaskCreateAPIView
 
 
 urlpatterns = [
-
+    path('api/v1/create-task/', TaskCreateAPIView.as_view(), name='task_create_api_view'),
 ]
