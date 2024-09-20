f5c95d82b1afbbdbf4170073159962598d3749d2
IliDevia <neiluamaibi@mail.ru>
2024-09-20 09:30:21 +0300

    AddDeadlineFieldInTaskModel

--- task_manager/models.py
+++ task_manager/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False, editable=True, verbose_name='Дата создания')
     description = models.TextField(null=False, blank=False, editable=True, verbose_name='Описание задачи')
+    deadline = models.DateTimeField(null=True, blank=True, editable=True, verbose_name='Дедлайн')
